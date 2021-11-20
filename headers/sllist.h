833949ac3216e659b700d771d756af12f1db6a88
attila-ks <dean.w95@protonmail.com>
2021-11-20 20:38:57 +0100

    Create Singly Linked List

---
+++ headers/sllist.h
@@ -0,0 +1,60 @@
+#ifndef SLLIST_H
+#define SLLIST_H
+
+#include <stdbool.h>
+
+typedef int Item;
+
+
+typedef struct SLList
+{
+  struct Node
+  {
+    Item item;
+    struct Node *next;
+  } *first, *last;
+  int size;
+  int operationCounter;
+} SLList;
+
+
+typedef struct ForwardIterator
+{
+  struct Node *current;
+  // Points to the operationCounter field of a SLList
+  const int *const sllistOperationCounterPtr;
+  // Used for comparison with sllistOperationCounterPtr field
+  const int currentSllistOperationCounter;
+} Iterator;
+
+
+typedef bool (*func)(const Item *const restrict item);
+
+
+// Constructs and returns an SLList with n elements. Each element is a copy.
+SLList sllistConstruct(int n, ...);
+void sllistDestruct(SLList *const restrict list);
+void sllistCopy(SLList *const dest, const SLList *const src);
+
+// Constructs and returns a fail-fast iterator, meaning no modifier function
+// should be called on SLList while using the iterator.
+Iterator sllistFirst(SLList *const restrict list);
+Iterator sllistLast(SLList *const restrict list);
+bool sllistHasNext(const Iterator *const restrict itr);
+Item *sllistNext(Iterator *const restrict itr);
+
+bool sllistIsEmpty(const SLList *const restrict list);
+int sllistSize(const SLList *const restrict list);
+
+void sllistPushFront(SLList *const restrict list,
+                     const Item *const restrict item);
+// Returns the popped item from the beginning of the SLList. list's size should
+// be tested before calling the function.
+Item sllistPopFront(SLList *const restrict list);
+void sllistPushBack(SLList *const restrict list,
+                    const Item *const restrict item);
+
+// Remove elements fulfilling condition
+void sllistRemove(SLList *const restrict list, func predicate);
+
+#endif // SLLIST_H
---
+++ src/sllist.c
@@ -0,0 +1,219 @@
+#include "../headers/sllist.h"
+#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+SLList sllistConstruct(int n, ...)
+{
+  // TODO: Is the following assert redundant to establish an invariant?
+  assert(n >= 0);
+
+  SLList list = {NULL, NULL, 0, 0};
+  va_list initList;
+  va_start(initList, n);
+
+  for (int i = 0; i < n; ++i)
+  {
+    Item item = va_arg(initList, int);
+    sllistPushBack(&list, &item);
+  }
+
+  va_end(initList);
+  return list;
+}
+
+
+void sllistDestruct(SLList *const restrict list)
+{
+  struct Node *current = list->first;
+
+  while (current)
+  {
+    list->first = list->first->next;
+    free(current);
+    current = list->first;
+  }
+
+  list->last = NULL;
+  list->size = 0;
+  list->operationCounter = 0;
+}
+
+
+void sllistCopy(SLList *const dest, const SLList *const src)
+{
+  sllistDestruct(dest);
+
+  const struct Node *current = src->first;
+
+  while (current)
+  {
+    sllistPushBack(dest, &(current->item));
+    current = current->next;
+  }
+}
+
+
+Iterator sllistFirst(SLList *const restrict list)
+{
+  return (Iterator)
+  {
+    list->first,
+    &(list->operationCounter),
+    list->operationCounter
+  };
+}
+
+
+Iterator sllistLast(SLList *const restrict list)
+{
+  return (Iterator)
+  {
+    list->last,
+    &(list->operationCounter),
+    list->operationCounter
+  };
+}
+
+
+bool sllistHasNext(const Iterator *const restrict itr)
+{
+  assert
+  (
+    *(itr->sllistOperationCounterPtr) == itr->currentSllistOperationCounter
+  );
+
+  return itr->current != NULL;
+}
+
+
+Item *sllistNext(Iterator *const restrict itr)
+{
+  assert
+  (
+    *(itr->sllistOperationCounterPtr) == itr->currentSllistOperationCounter
+  );
+
+  Item *item = &(itr->current->item);
+  itr->current = itr->current->next;
+  // TODO: Should it return NULL if itr points to NULL?
+  return item;
+}
+
+
+bool sllistIsEmpty(const SLList *const restrict list)
+{
+  return list->size == 0;
+}
+
+
+int sllistSize(const SLList *const restrict list)
+{
+  return list->size;
+}
+
+
+void sllistPushFront(SLList *const restrict list,
+                     const Item *const restrict item)
+{
+  struct Node *const first = (struct Node *const)malloc(sizeof(struct Node));
+  // TODO: Test against memory allocation failure!
+  first->item = *item;
+
+  if (list->size == 0)
+  {
+    first->next = NULL;
+    list->first = first;
+    list->last = first;
+  }
+  else
+  {
+    first->next = list->first;
+    list->first = first;
+  }
+
+  ++list->size;
+  ++list->operationCounter;
+}
+
+
+Item sllistPopFront(SLList *const restrict list)
+{
+  assert(list->size > 0);
+
+  struct Node *const oldFirst = list->first;
+  list->first = list->first->next;
+  Item item = oldFirst->item;
+  free(oldFirst);
+
+
+  if (list->size == 1)
+  {
+    list->last = NULL;
+  }
+
+  --list->size;
+  ++list->operationCounter;
+
+  return item;
+}
+
+
+void sllistPushBack(SLList *const restrict list,
+                    const Item *const restrict item)
+{
+  struct Node *const last = (struct Node *const)malloc(sizeof(struct Node));
+  // TODO: Test against memory allocation failure!
+  last->item = *item;
+  last->next = NULL;
+
+  if (list->size == 0)
+  {
+    list->first = last;
+    list->last = last;
+  }
+  else
+  {
+    list->last->next = last;
+    list->last = last;
+  }
+
+  ++list->size;
+  ++list->operationCounter;
+}
+
+
+// TODO: Can it be simplified?
+// TODO: Can it be more efficient? Because it creates a new SLList and items
+// are copied to the new list. Maybe implement and use
+// std::forward_list::erase_after -like function.
+void sllistRemove(SLList *const restrict list, func predicate)
+{
+  SLList newList = sllistConstruct(0);
+  struct Node *current = list->first;
+  int operationCounter = list->operationCounter;
+  bool modified = false;
+
+  while (current)
+  {
+    if (predicate(&(current->item)))
+    {
+      modified = true;
+      ++operationCounter;
+    }
+    else
+    {
+      sllistPushBack(&newList, &(current->item));
+    }
+
+    current = current->next;
+  }
+
+  if (modified)
+  {
+    sllistCopy(list, &newList);
+    list->operationCounter = operationCounter;
+  }
+
+  sllistDestruct(&newList);
+}
