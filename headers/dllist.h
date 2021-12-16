2191f3bc7b2011e239f69c2ac9ccbe664c2e206e
attila-ks <dean.w95@protonmail.com>
2021-12-16 20:36:49 +0100

    Create Doubly Linked List

---
+++ headers/dllist.h
@@ -0,0 +1,72 @@
+#ifndef DLLIST_H
+#define DLLIST_H
+
+#include <stdbool.h>
+
+typedef int Item;
+
+
+typedef struct DLList
+{
+  struct Node
+  {
+    Item item;
+    struct Node *prev;
+    struct Node *next;
+  } *first, *last;
+  int size;
+  int operationCounter;
+} DLList;
+
+
+typedef struct BidirectionalIterator
+{
+  DLList *list;
+  struct Node *current;
+  // Used for comparison with list.operationCounter field
+  int currentOperationCounter;
+} Iterator;
+
+
+typedef bool (*func)(const Item *const restrict item);
+
+
+// Constructs and returns an DLList with n elements. Each element is a copy.
+DLList dllistConstruct(int n, ...);
+void dllistDestruct(DLList *const restrict list);
+void dllistCopy(DLList *const dest, const DLList *const src);
+
+// Constructs and returns a fail-fast iterator, meaning no modifier function
+// should be called on DLList while using the iterator.
+Iterator dllistFirst(DLList *const restrict list);
+Iterator dllistLast(DLList *const restrict list);
+bool dllistHasNext(const Iterator *const restrict itr);
+// Returns a pointer to the next Item. Function dllistHasNext should be tested
+// before calling this function.
+Item *dllistNext(Iterator *const restrict itr);
+
+bool dllistIsEmpty(const DLList *const restrict list);
+int dllistSize(const DLList *const restrict list);
+
+void dllistPushFront(DLList *const restrict list,
+                     const Item *const restrict item);
+// Returns the popped Item from the beginning of the DLList. list's size should
+// be tested before calling the function.
+Item dllistPopFront(DLList *const restrict list);
+void dllistPushBack(DLList *const restrict list,
+                    const Item *const restrict item);
+// Returns the popped Item from the end of the DLList. list's size should
+// be tested before calling the function.
+Item dllistPopBack(DLList *const restrict list);
+// Inserts item before the Item at the specified position of the DLList.
+void dllistInsert(Iterator *const position, const Item *const restrict item);
+// Returns the removed Item from the specified position of the DLList. DLList's
+// size should be tested before calling the function.
+// Updates the iterator to point to the Item that followed the Item erased by
+// the function call.
+Item dllistErase(Iterator *const position);
+
+// Remove elements fulfilling condition
+void dllistRemoveIf(DLList *const restrict list, func predicate);
+
+#endif // DLLIST_H
---
+++ src/dllist.c
@@ -0,0 +1,304 @@
+#include "../headers/dllist.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <assert.h>
+
+DLList dllistConstruct(int n, ...)
+{
+  // TODO: Is the following assert redundant to establish an invariant?
+  assert(n >= 0);
+
+  DLList list = {NULL, NULL, 0, 0};
+  va_list initList;
+  va_start(initList, n);
+
+  for (int i = 0; i < n; ++i)
+  {
+    const Item item = va_arg(initList, Item);
+    dllistPushBack(&list, &item);
+  }
+
+  va_end(initList);
+  return list;
+}
+
+
+void dllistDestruct(DLList *const restrict list)
+{
+  struct Node *current = list->first;
+  while (current)
+  {
+    list->first = list->first->next;
+    free(current);
+    current = current->next;
+  }
+
+  list->last = NULL;
+  list->size = 0;
+  list->operationCounter = 0;
+}
+
+
+void dllistCopy(DLList *const dest, const DLList *const src)
+{
+  dllistDestruct(dest);
+
+  struct Node *current = src->first;
+  while (current)
+  {
+    dllistPushBack(dest, &(current->item));
+    current = current->next;
+  }
+}
+
+
+Iterator dllistFirst(DLList *const restrict list)
+{
+  return (Iterator)
+  {
+    list,
+    list->first,
+    list->operationCounter
+  };
+}
+
+
+Iterator dllistLast(DLList *const restrict list)
+{
+  return (Iterator)
+  {
+    list,
+    list->last,
+    list->operationCounter
+  };
+}
+
+
+bool dllistHasNext(const Iterator *const restrict itr)
+{
+  assert
+  (
+    itr->list->operationCounter == itr->currentOperationCounter
+  );
+
+  return itr->current != NULL;
+}
+
+
+Item *dllistNext(Iterator *const restrict itr)
+{
+  assert
+  (
+    itr->list->operationCounter == itr->currentOperationCounter
+  );
+
+  Item *item = &(itr->current->item);
+  itr->current = itr->current->next;
+  return item;
+}
+
+
+bool dllistIsEmpty(const DLList *const restrict list)
+{
+  return list->size == 0;
+}
+
+
+int dllistSize(const DLList *const restrict list)
+{
+  return list->size;
+}
+
+
+void dllistPushFront(DLList *const restrict list,
+                     const Item *const restrict item)
+{
+  // TODO: Test against memory allocation failure!
+  struct Node *const first = (struct Node *const)malloc(sizeof(struct Node));
+  first->item = *item;
+  first->prev = NULL;
+
+  if (list->size == 0)
+  {
+    first->next = NULL;
+    list->first = first;
+    list->last = first;
+  }
+  else
+  {
+    list->first->prev = first;
+    first->next = list->first;
+    list->first = first;
+  }
+
+  ++list->size;
+  ++list->operationCounter;
+}
+
+
+Item dllistPopFront(DLList *const restrict list)
+{
+  assert(list->size > 0);
+
+  struct Node *const oldFirst = list->first;
+  const Item item = oldFirst->item;
+  list->first = list->first->next;
+  free(oldFirst);
+
+  --list->size;
+  ++list->operationCounter;
+
+  if (list->size == 0)
+  {
+    list->last = NULL;
+  }
+  else
+  {
+    list->first->prev = NULL;
+  }
+
+  return item;
+}
+
+
+void dllistPushBack(DLList *const restrict list,
+                    const Item *const restrict item)
+{
+  // TODO: Test against memory allocation failure!
+  struct Node *const last = (struct Node *const)malloc(sizeof(struct Node));
+  last->item = *item;
+  last->next = NULL;
+
+  if (list->size == 0)
+  {
+    last->prev = NULL;
+    list->first = last;
+    list->last = last;
+  }
+  else
+  {
+    list->last->next = last;
+    last->prev = list->last;
+    list->last = last;
+  }
+
+  ++list->size;
+  ++list->operationCounter;
+}
+
+
+Item dllistPopBack(DLList *const restrict list)
+{
+  assert(list->size > 0);
+
+  struct Node *oldLast = list->last;
+  const Item item = oldLast->item;
+  list->last = list->last->prev;
+  free(oldLast);
+
+  --list->size;
+  ++list->operationCounter;
+
+  if (list->size == 0)
+  {
+    list->first = NULL;
+  }
+  else
+  {
+    list->last->next = NULL;
+  }
+
+  return item;
+}
+
+
+void dllistInsert(Iterator *const position, const Item *const restrict item)
+{
+  if (position->list->size == 0 || position->current == position->list->first)
+  {
+    dllistPushFront(position->list, item);
+  }
+  else
+  {
+    // TODO: Test against memory allocation failure!
+    struct Node *const node = (struct Node *const)malloc(sizeof(struct Node));
+    node->item = *item;
+    node->prev = position->current->prev;
+    position->current->prev->next = node;
+    position->current->prev = node;
+    node->next = position->current;
+
+    ++position->list->size;
+    ++position->list->operationCounter;
+  }
+}
+
+
+// TODO: Test whether iterator is already at the end of the list?!
+Item dllistErase(Iterator *const position)
+{
+  assert(position->list->size > 0);
+
+  Item item;
+
+  if (position->current == position->list->first)
+  {
+    position->current = position->current->next;
+    item = dllistPopFront(position->list);
+  }
+  else if (position->current == position->list->last)
+  {
+    position->current = position->current->next;
+    item = dllistPopBack(position->list);
+  }
+  else
+  {
+    struct Node *const oldCurrent = position->current;
+    position->current = position->current->next;
+    oldCurrent->prev->next = oldCurrent->next;
+    oldCurrent->next->prev = oldCurrent->prev;
+    item = oldCurrent->item;
+    free(oldCurrent);
+
+    --position->list->size;
+    ++position->list->operationCounter;
+  }
+
+  return item;
+}
+
+
+// TODO: Can it be simplified?
+// TODO: Can it be more efficient? Because it creates a new DLList and items
+// are copied to the new list. Maybe implement and use
+// std::forward_list::erase_after -like function.
+void dllistRemoveIf(DLList *const restrict list, func predicate)
+{
+  DLList newList = dllistConstruct(0);
+  struct Node *current = list->first;
+  int operationCounter = list->operationCounter;
+  bool modified = false;
+
+  while (current)
+  {
+    if (predicate(&(current->item)))
+    {
+      modified = true;
+      ++operationCounter;
+    }
+    else
+    {
+      dllistPushBack(&newList, &(current->item));
+    }
+
+    current = current->next;
+  }
+
+  if (modified)
+  {
+    dllistCopy(list, &newList);
+    list->operationCounter = operationCounter;
+  }
+
+  dllistDestruct(&newList);
+}
